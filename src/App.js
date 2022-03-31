ae27022cf6a3ae5583c79ab96e1cad8ce5cb7c44
Animesh-weavers <animesh.dey@weavers-web.com>
2022-03-31 16:32:41 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import './App.css';
+import DropFileInput from './components/drop-file-input/DropFileInput';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+function App() {
+
+    const onFileChange = (files) => {
+        console.log(files);
+    }
+
+    return (
+        <div className="box">
+            <h2 className="header">
+                React drop files input
+            </h2>
+            <DropFileInput
+                onFileChange={(files) => onFileChange(files)}
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
